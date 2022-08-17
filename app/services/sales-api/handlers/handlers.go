f68c43474a20ed371dd3238d0b97fefa026413dc
Naveen Kunareddy <gosmartwizard@gmail.com>
2022-08-17 11:30:44 +0530

    Added Custom Router

--- app/services/sales-api/handlers/handlers.go
+++ app/services/sales-api/handlers/handlers.go
@@ -8,9 +8,9 @@ import (
 	"net/http/pprof"
 	"os"
 
-	"github.com/dimfeld/httptreemux/v5"
 	"github.com/gosmartwizard/WebService/app/services/sales-api/handlers/debug/checkgrp"
 	"github.com/gosmartwizard/WebService/app/services/sales-api/handlers/v1/testgrp"
+	"github.com/gosmartwizard/WebService/foundation/web"
 	"go.uber.org/zap"
 )
 
@@ -57,14 +57,14 @@ type APIMuxConfig struct {
 }
 
 // APIMux constructs an http.Handler with all application routes defined.
-func APIMux(cfg APIMuxConfig) *httptreemux.ContextMux {
-	mux := httptreemux.NewContextMux()
+func APIMux(cfg APIMuxConfig) *web.App {
+	app := web.NewApp(cfg.Shutdown)
 
 	tgh := testgrp.Handlers{
 		Log: cfg.Log,
 	}
 
-	mux.Handle(http.MethodGet, "/v1/test", tgh.Test)
+	app.Handle(http.MethodGet, "/v1/test", tgh.Test)
 
-	return mux
+	return app
 }
